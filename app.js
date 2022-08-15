5d8c15ee215ce5bc63751e7afb267a0f728eb69d
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2022-08-15 18:31:49 +1200

    Restarted

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-sizeOfGrid = 100;
+let sizeOfGrid = 50;
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('button');
 
 //nested for loops to make the grid
 
 const createGrid = (amtOfGrids) => {
+    // const wrapper = document.createElement('div');
+    // wrapper.classList.add('wrapper');
+
     for (let i = 0; i < amtOfGrids; i++){
         const row = document.createElement('div')
         row.classList.add('grid-row');
 // within this loop the gridbox is given its style
 // the size of the cubes is relative to the size of the box
         for (let j = 0; j < amtOfGrids; j++){
-            const widthAndHeight = 960 / sizeOfGrid;
+            const widthAndHeight = 960 / amtOfGrids;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
             gridBox.style.width = `${widthAndHeight}px`;
@@ -19,14 +23,37 @@ const createGrid = (amtOfGrids) => {
             //event listerner to hightlight the boxes
             gridBox.addEventListener('mouseenter', () => {
                 gridBox.style.backgroundColor = 'black';
-            });
-
+            })
             row.appendChild(gridBox);
-        };
+        }
 
         container.appendChild(row);
-    };
+    }
+    // container.appendChild(wrapper);
 }
 
-createGrid(sizeOfGrid);
+
+
+//button to reset grid
+resetButton.addEventListener('click', () => {
+    const userSize = Number(prompt('What dimensions do you want for the grid?'));
+
+    while (userSize > 100) {
+        userSize = Number(prompt('Pick again, has to be less than 100. '));
+    }
+    
+    // const wrapper = document.querySelector('.wrapper');
+
+    // if(!wrapper){
+    //     createGrid(userSize);
+
+    // }else {
+    //     wrapper.remove();
+
+    // }
+
+    
+})
+
+// createGrid(sizeOfGrid);
 
---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// let board = document.querySelector('.board');
+// board.getElementsByClassName.gridTemplateColumns = "repeat(16, fr1)";
+// board.getElementsByClassName.gridTemplateRows = "repeat(16, fr1)";
+ 
+// for (let i = 0; i<256; i++){
+//     let square = document.createElement
+// }
